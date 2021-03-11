2612bbd54e9f323235b11b1b38188c7aedd62d50
Arielle Ross-Levy <ariellerosslevy@Arielles-MacBook-Air.local>
2021-03-11 14:48:17 -0700

    added contact page

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -3,20 +3,11 @@ import React from 'react'
 export default function Contact() {
     return (
         <div>
-            <h1 className="page-title">Contact</h1>
+            <h1 className="page-title">World Domination Plan?</h1>
                 <p>
-                    I am a full-stack web developer with experience in HTML, CSS, JS, SQL, Ruby, Rails, and React 
-                    (pst... this site was built on React and Rails!)
+                    Let's talk details. Email me at arielle@arielleross.com
                 </p>
-                <p>
-                    I graduated from New York University with a degree in Experiential Technology. 
-                    After working in project management at startups, I pivoted to web development 
-                    so that I could dig my hands in and solve product puzzles myself.
-                </p>
-                <p>
-                    In my spare time, I sing along to musical theater soundtracks and work on whatever DIY craft
-                    the season calls for. (At the moment, that's converting my cargo trailer, Iris, into a camper van.)
-                </p>
+
         </div>
     )
 }
